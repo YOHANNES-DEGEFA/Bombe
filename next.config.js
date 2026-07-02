17a5af80336e5ca89554005da102d7ebd8849f4d
YOHANNES-DEGEFA <yohdeforemost@gmail.com>
2026-07-02 20:40:23 +0300

    clean mob version

--- next.config.js
+++ next.config.js
@@ -17,35 +17,6 @@ const nextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/tv.apk',
-        destination: '/api/download-apk?type=tv',
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: '/downloads/Bombe.apk',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/vnd.android.package-archive',
-          },
-          {
-            key: 'Content-Disposition',
-            value: 'attachment; filename="Bombe.apk"',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
